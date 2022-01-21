ab11d4233cf15ae40ad1ead5f97e3960faf9bd71
mendez <ikermendezlopez@gmail.com>
2022-01-21 10:00:40 +0100

    mapa y eso

---
+++ angular/appMapaLocalStorage/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material.module';
+import { MapaComponent } from './components/mapa/mapa.component';
+import {AgmCoreModule} from '@agm/core';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MapaComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatSliderModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDY3YQ6aNb7YXoz13RIxq5fVyzdbak7sQ0'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
